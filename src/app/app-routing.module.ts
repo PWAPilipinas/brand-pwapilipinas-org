7de17dde64eda7c63442ad1416a1f60c3667f32f
Andresito de Guzman <andresitomyemail@gmail.com>
2021-08-30 12:49:14 +0800

    feat: update root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { DownloadComponent } from './page/download/download.component';
+import { LogoComponent } from './page/logo/logo.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'downloads', component: DownloadComponent },
+  { path: 'logo', component: LogoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
